e058954c579f6023f47dffa712242005bde93e4c
KarmicFuture <79309217+KarmicFuture@users.noreply.github.com>
2021-12-26 12:15:01 -0500

    Updated for code adherence

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 #convert decimal number into binary
 
-decimal_number = 432
+def Dec_Num(decimal_number):
+    while int(decimal_number) >> 0:
+        mod = decimal_number % 2
+        decimal_number = decimal_number // 2
+        print(decimal_number, "    ", mod)
+        binary_list.append(mod)
+
+
 binary_list = []
-while (int(decimal_number) >> 0):
-    mod = decimal_number % 2
-    decimal_number = decimal_number// 2
-    print(decimal_number, "    ", mod)
-    binary_list.append(mod)
+
+number = input("What is the number you want to convert?   ")
+decimal_number =int(number)
+
+Dec_Num(decimal_number)
 
 print(binary_list[::-1])
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
